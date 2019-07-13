99102e9cfe1cb330d9e229a795c1e2343980895c
maxxie114 <maxxie114@gmail.com>
2019-07-12 22:54:08 -0700

    Initial Commit

---
+++ AntiSwearAPI/src/main/java/org/mxpersonal/AntiSwearAPI/App.java
@@ -0,0 +1,13 @@
+package org.mxpersonal.AntiSwearAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
